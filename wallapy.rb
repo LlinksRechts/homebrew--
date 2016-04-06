70249dad1cc9758810e90884faa142cb5bab4043
LlinksRechts <rys15527@spengergasse.at>
2016-04-06 20:51:08 +0200

    changed hash because forgot before

--- wallapy.rb
+++ wallapy.rb
@@ -2,7 +2,7 @@ class Wallapy < Formula
   desc "Walla.py is the library that supplies utility functions for the Walla.py Eclipse plugin. It is used to prepare a Wallaby for use with the plugin, as well as retrieve any connected Wallabys."
   homepage "http://wallapy.lukor.tk"
   url "https://www.lukor.tk/files/wallapy.tar.gz"
-  sha256 "841b5842045201fb6207d6beb6c0a546ba5bcfb072870e7f3b7a84868b491c75"
+  sha256 "d98c7a1887fdf276dcfa36894aa5aa5dcb989f852d49c2a120ed13426cc182f3"
   version "1.1.2"
 
   depends_on "nmap"
